fe88abc2158a1c57a034d97d6bc3927321a67a83
dwhelan <dwhelan@dpwhelan.com>
2015-08-12 12:32:04 -0400

    Added initial support for delegating to attributes.

---
+++ spec/delegate_matcher.rb
@@ -0,0 +1,113 @@
+# RSpec matcher to spec delegations.
+# From https://gist.github.com/awinograd/6158961
+# Forked from https://gist.github.com/ssimeonov/5942729 with fixes
+# for arity + custom prefix.
+#
+# Usage:
+#
+#     describe Post do
+#       it { should delegate(:name).to(:author).with_prefix } # author_name => author.name
+#       it { should delegate(:name).to(:author).with_prefix(:any) } # any_name => author.name
+#       it { should delegate(:month).to(:created_at) }
+#       it { should delegate(:year).to(:created_at) }
+#       it { should delegate(:something).to(:'@instance_var') }
+#     end
+RSpec::Matchers.define :delegate do |method|
+  match do |delegator|
+    @method = @prefix ? :"#{@prefix}_#{method}" : method
+    @delegator = delegator
+
+    if delegate_is_an_attribute?
+      delegate_to_attribute(method)
+    else
+      raise "#{@delegator} does not respond to #{@to}" unless @delegator.respond_to?(@to, true)
+      delegate_to_method(method)
+    end
+  end
+
+  description do
+    "delegate :#{@method} to its #{@to}#{@prefix ? ' with prefix' : ''}"
+  end
+
+  failure_message do |text|
+    "expected #{@delegator} to delegate :#{@method} to its #{@to}#{@prefix ? ' with prefix' : ''}"
+  end
+
+  failure_message_when_negated do |text|
+    "expected #{@delegator} not to delegate :#{@method} to its #{@to}#{@prefix ? ' with prefix' : ''}"
+  end
+
+  chain(:to)          { |receiver|   @to = receiver }
+  chain(:with_prefix) { |prefix=nil| @prefix = prefix || @to }
+
+  private
+
+  def delegate_is_an_attribute?
+    @to.to_s[0] == '@'
+  end
+
+  def delegate_to_attribute(method)
+    original_to = @delegator.instance_variable_get(@to)
+    begin
+      @delegator.instance_variable_set(@to, delegate_double(method))
+      @delegator.send(@method) == :called
+    ensure
+      @delegator.instance_variable_set(@to, original_to)
+    end
+  end
+
+  def delegate_to_method(method)
+    unless [0, -1].include?(@delegator.method(@to).arity)
+      raise "#{@delegator}'s' #{@to} method does not have zero or -1 arity (it expects parameters)"
+    end
+    @delegator.stub(@to).and_return delegate_double(method)
+    @delegator.send(@method) == :called
+  end
+
+  def delegate_double(method)
+    double('receiver').tap do |receiver|
+      receiver.stub(method).and_return :called
+    end
+  end
+end
+
+# RSpec::Matchers.define :delegate do |method|
+#   match do |delegator|
+#     @method = @prefix ? :"#{@to}_#{delegate_method}" : delegate_method
+#     @delegator = delegator
+#
+#     if @to.is_a? Symbol
+#       begin
+#         @delegator.send(@to)
+#       rescue NoMethodError
+#         raise "#{@delegator} does not respond to #{@to}!"
+#       end
+#       @delegator.stub(@to).and_return double('receiver')
+#       @delegator.send(@to).stub(method).and_return :called
+#     else
+#       @to.stub(delegate_method) { :called }
+#     end
+#
+#     @delegator.send(method) == :called
+#   end
+#
+#   def delegate_method
+#     @as || @prefix ? :"#{@to}_#{@method}" : @method
+#   end
+#
+#   description do
+#     "delegate :#{@method} to #{@to}#{@prefix ? ' with prefix' : ''}"
+#   end
+#
+#   failure_message do |text|
+#     "expected #{@delegator} #{x} to delegate :#{method} to #{@to}#{@prefix ? ' with prefix' : ''}"
+#   end
+#
+#   failure_message_when_negated do |text|
+#     "expected #{@delegator} not to delegate :#{method} to #{@to}#{@prefix ? ' with prefix' : ''}"
+#   end
+#
+#   chain(:to) { |receiver| @to = receiver }
+#   chain(:as) { |method| @as = method }
+#   chain(:with_prefix) { @prefix = true }
+#  end
--- spec/lib/delegate_matcher_spec.rb
+++ spec/lib/delegate_matcher_spec.rb
@@ -15,46 +15,60 @@ describe 'Delegate matcher' do
   end
 
   context 'simple delegation' do
-    it 'with no prefix should call same method' do
-      post.class.class_eval do
-        def name
-          author.name
+    context 'to attribute' do
+      it 'with no prefix should call same method' do
+        post.class.class_eval do
+          def name
+            @author.name
+          end
         end
-      end
-      expect(post).to delegate(:name).to(:author)
-    end
-
-    it 'with unspecified prefix should call method with prefix of delegate name' do
-      post.class.class_eval do
-        def author_name
-          author.name
-        end
-      end
-      expect(post).to delegate(:name).to(:author).with_prefix
-    end
-
-    it 'with specified prefix should call method with prefix ' do
-      post.class.class_eval do
-        def writer_name
-          author.name
-        end
-      end
-      expect(post).to delegate(:name).to(:author).with_prefix(:writer)
-    end
-
-    it 'with an invalid "to" should raise' do
-      expect { expect(post).to delegate(:name).to(:invalid_delegate) }.to raise_error do |exception|
-        expect(exception.message).to match /does not respond to invalid_delegate/
+        expect(post).to delegate(:name).to(:@author)
       end
     end
 
-    it 'with arg defined for method should raise' do
-      post.class.class_eval do
-        def author(salutation)
+    context 'to method' do
+
+      it 'with no prefix should call same method' do
+        post.class.class_eval do
+          def name
+            author.name
+          end
+        end
+        expect(post).to delegate(:name).to(:author)
+      end
+
+      it 'with unspecified prefix should call method with prefix of delegate name' do
+        post.class.class_eval do
+          def author_name
+            author.name
+          end
+        end
+        expect(post).to delegate(:name).to(:author).with_prefix
+      end
+
+      it 'with specified prefix should call method with prefix ' do
+        post.class.class_eval do
+          def writer_name
+            author.name
+          end
+        end
+        expect(post).to delegate(:name).to(:author).with_prefix(:writer)
+      end
+
+      it 'with an invalid "to" should raise' do
+        expect { expect(post).to delegate(:name).to(:invalid_delegate) }.to raise_error do |exception|
+          expect(exception.message).to match /does not respond to invalid_delegate/
         end
       end
-      expect { expect(post).to delegate(:name).to(:author) }.to raise_error do |exception|
-        expect(exception.message).to match /author method does not have zero or -1 arity/
+
+      it 'with arg defined for method should raise' do
+        post.class.class_eval do
+          def author(salutation)
+          end
+        end
+        expect { expect(post).to delegate(:name).to(:author) }.to raise_error do |exception|
+          expect(exception.message).to match /author method does not have zero or -1 arity/
+        end
       end
     end
   end
