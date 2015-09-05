52ba6b290c5dc04fff696aab99d51720024e4269
dwhelan <dwhelan@dpwhelan.com>
2015-09-05 10:10:55 -0400

    Add access support to attribute matcher.

--- spec/have_attribute_matcher.rb
+++ spec/have_attribute_matcher.rb
@@ -1,66 +1,94 @@
 RSpec::Matchers.define(:have_attribute) do |attribute|
-  match do |object|
+  match do
     @attribute = attribute
-    @object = object
-    readable_ok? && writeable_ok?
+    readability_ok? && writeability_ok?
+  end
+
+  def initialize(*)
+    super
+  end
+
+  chain(:readonly)   { @expected_readable  = true; @expected_writeable = false }
+  chain(:writeonly)  { @expected_writeable = true; @expected_readable  = false }
+  chain(:read_write) { @expected_writeable = true; @expected_readable  = true }
+
+  def description
+    format('have %sattribute %p', access_description, attribute)
   end
 
   private
 
-  attr_reader :object, :attribute
+  attr_reader :attribute
 
-  def readable_ok?
-    readable? == expected_readable?
+  def exists?
+    readable? || writeable?
+  end
+
+  def readability_ok?
+    @expected_readable.nil? || readable? == expected_readable?
   end
 
   def readable?
-    object.respond_to?(attribute)
+    actual.respond_to?(attribute)
   end
 
   def expected_readable?
-    @expected_readable ||= true
+    @expected_readable.nil? ? true : @expected_readable
   end
 
-  def writeable_ok?
-    writeable? == expected_writeable?
+  def writeability_ok?
+    @expected_writeable.nil? || writeable? == expected_writeable?
   end
 
   def writeable?
-    object.respond_to?("#{attribute}=")
+    actual.respond_to?("#{attribute}=")
   end
 
   def expected_writeable?
-    @expected_writeable ||= true
+    @expected_writeable.nil? ? true : @expected_writeable
   end
 
-  def failure_message
-    message = failure_message_details(false)
-    message.empty? ? super : message
-  end
-
-  def failure_message_when_negated
-    message = failure_message_details(true)
-    message.empty? ? super : message
-  end
-
-  def failure_message_details(negated)
-    [
-        readonly_message(negated),
-    ].reject(&:empty?).join(' and ')
-  end
-
-  def readonly_message(negated)
+  def access_description
     case
-    when !readable?
+    when @expected_readable.nil? && @expected_writeable.nil?
       ''
-    when negated ^ readable_ok?
-      ''
-    when negated
-      'is not read only'
+    when expected_readable? && expected_writeable?
+      'read/write '
+    when expected_readable?
+      'read only '
     else
-      'is read only'
+      'write only '
     end
   end
+
+  # def failure_message
+  #   super
+  #   # exists? ? failure_message_details(false, super) : super
+  # end
+  #
+  # def failure_message_when_negated
+  #   super
+  #   # exists? ? super : failure_message_details(true, super)
+  # end
+
+  # def failure_message_details(negated, default)
+  #   message = [
+  #       readonly_message(negated),
+  #   ].reject(&:empty?).join(' and ')
+  #
+  #   message.empty? ? default : message
+  # end
+
+  # def readonly_message(negated)
+  #   case
+  #   when @expected_readable.nil? || negated ^ readability_ok?
+  #     ''
+  #   when negated
+  #     'is not read only'
+  #   else
+  #     'is read only'
+  #   end
+  # end
 
   # description do
   #   "foo"
--- spec/lib/have_attribute_spec.rb
+++ spec/lib/have_attribute_spec.rb
@@ -6,30 +6,68 @@ describe 'have_attribute matcher' do
     Class.new do
       attr_accessor :rw
       attr_reader   :r
+      attr_writer   :w
     end
   end
 
   subject { klass.new }
 
-  it { is_expected.to have_attribute(:rw) }
-  it { is_expected.not_to have_attribute(:r) }
+  describe 'attr_accessor :rw' do
+    it { is_expected.to     have_attribute(:rw)            }
+    it { is_expected.to     have_attribute(:rw).read_write }
+    it { is_expected.not_to have_attribute(:rw).readonly   }
+    it { is_expected.not_to have_attribute(:rw).writeonly  }
+  end
 
-  describe 'description' do
+  describe 'attr_reader :r' do
+    it { is_expected.to     have_attribute(:r)            }
+    it { is_expected.to     have_attribute(:r).readonly   }
+    it { is_expected.not_to have_attribute(:r).writeonly  }
+    it { is_expected.not_to have_attribute(:r).read_write }
+  end
+
+  describe 'attr_writer :w' do
+    it { is_expected.to     have_attribute(:w)            }
+    it { is_expected.to     have_attribute(:w).writeonly  }
+    it { is_expected.not_to have_attribute(:w).readonly   }
+    it { is_expected.not_to have_attribute(:w).read_write }
+  end
+
+  describe 'messaging' do
     let(:matcher) { self.class.parent_groups[1].description }
     subject       { eval matcher }
     before        { subject.matches? klass.new }
 
-    context 'have_attribute(:missing)' do
-      its(:description)     { should eq 'have attribute :missing' }
-      its(:failure_message) { should match /have attribute :missing/ }
+    {
+        :'have_attribute(:name)'            => 'have attribute :name',
+        :'have_attribute(:name).readonly'   => 'have read only attribute :name',
+        :'have_attribute(:name).writeonly'  => 'have write only attribute :name',
+        :'have_attribute(:name).read_write' => 'have read/write attribute :name',
+        :'have_attribute(:rw).readonly'     => 'have read only attribute :rw',
+        :'have_attribute(:rw).writeonly'    => 'have write only attribute :rw',
+        :'have_attribute(:w).readonly'      => 'have read only attribute :w',
+        :'have_attribute(:r).writeonly'     => 'have write only attribute :r',
+
+    }.each do |expectation, expected_description|
+        describe(expectation) do
+          its(:description)     { is_expected.to eq expected_description }
+          its(:failure_message) { is_expected.to match /expected .+ to #{expected_description}/ }
+          its(:failure_message_when_negated) { is_expected.to match /expected .+ not to #{expected_description}/ }
+        end
     end
 
-    context 'have_attribute(:name)' do
-      its(:failure_message_when_negated) { should match /not to have attribute :name/ }
+    {
+        :'have_attribute(:rw)'          => 'expected .+ not to have attribute :rw',
+        :'have_attribute(:r).readonly'  => 'expected .+ not to have read only attribute :r',
+        :'have_attribute(:w).writeonly' => 'expected .+ not to have write only attribute :w'
+    }.each do |expectation, expected_description|
+      describe(expectation) do
+        its(:failure_message_when_negated) { is_expected.to match /#{expected_description}/ }
+      end
     end
 
-    xcontext 'have_attribute(:r)' do
-      its(:failure_message)  { should match /attribute :r is read_only/ }
+    context 'have_attribute(:rw)' do
+      its(:failure_message_when_negated) { is_expected.to match /not to have attribute :rw/ }
     end
   end
 end
